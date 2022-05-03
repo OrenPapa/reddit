a5703d51a8853fbc6261861a048d7a8448fa808b
User <orennpapa@gmail.com>
2022-05-03 19:11:18 +0300

    Add subreddit custom hook

---
+++ src/Hooks/useSubreddit.ts
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+function useSubreddit(url: string) {
+  const [data, setData] = useState([{title: "", description: ""}]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get(url)
+      .then((response) => {
+        setData(response.data);
+      })
+      .catch((err) => {
+        setError(err);
+      })
+      .finally(() => setLoading(false));
+  }, [url]);
+
+  return { data, loading, error };
+}
+
+export default useSubreddit;
--- src/Module/Pages/Homepage.tsx
+++ src/Module/Pages/Homepage.tsx
@@ -2,13 +2,30 @@ import React from "react";
 import Navbar from "../Components/Navbar";
 import "../../Styles/main.scss";
 import SubredditCard from "../Components/SubredditCard";
+import useSubreddit from "../../Hooks/useSubreddit";
 
 function Homepage() {
+  const { data, loading, error } = useSubreddit(
+    "https://6040c786f34cf600173c8cb7.mockapi.io/subreddits"
+  );
+
   return (
     <>
       <Navbar />
       <div className="homepage">
-        <SubredditCard title="Reddit" description="this is a short subreddit description" />
+        {loading && <h2>Loading...</h2>}
+        {error && <h2>An error has occured please refresh your page.</h2>}
+        {!loading &&
+          !error &&
+          data.map((item: any) => {
+            return (
+              <SubredditCard
+                key={item.id}
+                title={item.title}
+                description={item.description}
+              />
+            );
+          })}
       </div>
     </>
   );
