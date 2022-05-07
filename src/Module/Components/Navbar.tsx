d419cfb09e07a91d63ae7bd2e3635512a585f962
User <orennpapa@gmail.com>
2022-05-07 09:31:26 +0300

    Add navbar navigation to home and fix scss

--- src/Module/Components/Navbar.tsx
+++ src/Module/Components/Navbar.tsx
@@ -1,10 +1,14 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import "../../Styles/Components/Navbar.scss";
 
 function Navbar(props: { pageTitle?: string; adminName?: string }) {
+  const navigate = useNavigate();
   return (
     <div className="navbar">
-      <div className="navbar__title">Reddit</div>
+      <div onClick={() => navigate("/")} className="navbar__title">
+        Reddit
+      </div>
       <div className="navbar__page-title">{props.pageTitle}</div>
       <div className="navbar__admin-name">{props.adminName}</div>
     </div>
