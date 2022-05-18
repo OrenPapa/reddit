e52eb62afd35c7228ee4aaab619a490ae28ccd52
unknown <orennpapa@gmail.com>
2022-05-18 09:58:11 +0200

    Manage api call with redux thunk

--- src/Module/Pages/Homepage.tsx
+++ src/Module/Pages/Homepage.tsx
@@ -1,27 +1,30 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "../Components/Navbar";
 import "../../Styles/main.scss";
 import SubredditCard from "../Components/SubredditCard";
-import useSubreddit from "../../Hooks/useSubreddit";
 import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
-import { RootState } from "../../Redux/Store";
+import { AppDispatch, RootState } from "../../Redux/Store";
+import { getSubreddits } from "../../Redux/SubredditSlice";
+import { useDispatch } from "react-redux";
 
 function Homepage() {
   const navigate = useNavigate();
   const [pageNumber, setPageNumber] = useState(1);
-  const { loadingSubbredits, subredditError, hasMore } = useSubreddit(
-    `https://6040c786f34cf600173c8cb7.mockapi.io/subreddits?page=${pageNumber}&limit=16`
-  );
+  const dispatch = useDispatch<AppDispatch>();
   const subredditsState = useSelector(
-    (state: RootState) => state.subredditsSlice.subreddits
+    (state: RootState) => state.subredditsSlice
   );
+
+  useEffect(() => {
+    dispatch(getSubreddits(pageNumber));
+  }, [dispatch, pageNumber]);
 
   window.addEventListener("scroll", () => {
     if (
       window.scrollY + window.innerHeight >=
         document.documentElement.scrollHeight &&
-      hasMore
+      subredditsState.hasMore
     ) {
       setPageNumber(pageNumber + 1);
     }
@@ -32,7 +35,7 @@ function Homepage() {
       <Navbar pageTitle="subreddits" />
       <div className="homepage">
         <div className="homepage__subreddits">
-          {subredditsState?.map((item) => {
+          {subredditsState.subreddits?.map((item) => {
             return (
               <div key={item.id}>
                 <SubredditCard
@@ -47,9 +50,9 @@ function Homepage() {
         </div>
       </div>
       <div className="homepage__text">
-        {loadingSubbredits && <h2>Loading...</h2>}
-        {subredditError && (
-          <h2>An error has occured please refresh your page.</h2>
+        {subredditsState.isLoading && <h2>Loading...</h2>}
+        {subredditsState.hasError && (
+          <h2>An error has occurred please refresh your page.</h2>
         )}
       </div>
     </>
--- src/Redux/SubredditSlice.ts
+++ src/Redux/SubredditSlice.ts
@@ -1,21 +1,43 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
+import axios from "axios";
 import { getUniqueObjects } from "../Helpers/Helpers";
-import { Subreddit } from "../Types/Subreddits";
+import { Subreddit, SubredditsResponse } from "../Types/Subreddits";
 
-type SubredditsArray = {
+export const getSubreddits = createAsyncThunk(
+  "subreddits/getSubreddits",
+  async (pageNumber: number) => {
+    const response: SubredditsResponse = await axios.get(`https://6040c786f34cf600173c8cb7.mockapi.io/subreddits?page=${pageNumber}&limit=16`)
+    return response.data
+  }
+)
+
+type SubredditsState = {
   subreddits: Subreddit[];
+  isLoading: boolean;
+  hasError: boolean;
+  hasMore: boolean
 };
 
-const initialState: SubredditsArray = { subreddits: [] };
+const initialState: SubredditsState = { subreddits: [], isLoading: true, hasError: false, hasMore: false };
 
 const SubredditsSlice = createSlice({
   name: "SubredditsSlice",
   initialState: initialState,
-  reducers: {
-    setSubredditsData(state, action: PayloadAction<Subreddit[]>) {
-      state.subreddits = getUniqueObjects([...state.subreddits, ...action.payload]);
-    },
-  },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+    .addCase(getSubreddits.pending, (state: SubredditsState) => {
+      state.isLoading = true;
+    },)
+    .addCase(getSubreddits.fulfilled, (state: SubredditsState, action: PayloadAction<Subreddit[]>) => {
+     state.subreddits = getUniqueObjects([...state.subreddits, ...action.payload])
+     state.hasMore = action.payload.length > 0; 
+      state.isLoading = false;
+      console.log(state.hasMore)
+    })
+    .addCase(getSubreddits.rejected, (state: SubredditsState) => {
+      state.hasError = true;
+      state.isLoading = false;
+    }) }
 });
-export const { setSubredditsData } = SubredditsSlice.actions;
 export default SubredditsSlice.reducer;
